924792549223056709040270a576b2f4f62c6410
JhojanCA <jhojanalexca12@gmail.com>
2023-12-07 12:47:34 -0500

    Se crean los componentes necesarios y el navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { MainComponent } from './components/main/main.component';
+import { BookingComponent } from './components/booking/booking.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainComponent,
+    BookingComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
