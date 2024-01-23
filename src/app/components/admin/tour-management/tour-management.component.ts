a248d85378edcddf265b2a4d7260833066e6ea55
JhojanCA <jhojanalexca12@gmail.com>
2024-01-23 13:59:16 -0500

    se modificaron cosas xd

--- src/app/components/admin/tour-management/tour-management.component.ts
+++ src/app/components/admin/tour-management/tour-management.component.ts
@@ -1,6 +1,8 @@
 import { Component, inject, TemplateRef } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Tour } from 'src/app/interfaces/tour';
+import { TourService } from 'src/app/services/tour.service';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -10,9 +12,35 @@ import Swal from 'sweetalert2';
 })
 export class TourManagementComponent {
   private modal = inject(NgbModal);
+  form: FormGroup;
   listTours: Tour[] = [];
+  loading: boolean = false;
+
+  constructor(private _tourService: TourService,
+              private fb: FormBuilder) {
+
+    this.form = this.fb.group({
+      nombre: ['', Validators.required],
+      precio: [null, Validators.required],
+      id_categoria: [null, Validators.required],
+      image: [null, Validators.required],
+      descripcion: ['', Validators.required],
+    });
+  }
+
+  ngOnInit(): void {
+    this.getListTours();
+  }
+
+  getListTours() {
+    this.loading = true;
+    this._tourService.getListTours().subscribe((data: Tour[]) => {
+      this.listTours = data;
+      this.loading = false;
+    })
+  }
   
-  openModal(content: TemplateRef<any>) {
+  openModal(content: TemplateRef<any>, id: number) {
     this.modal.open(content, { windowClass: 'dark-modal', size: 'lg' });
   }
 
--- src/app/interfaces/tour.ts
+++ src/app/interfaces/tour.ts
@@ -1,5 +1,6 @@
 export interface Tour {
     id?: number;
+    id_categoria: number;
     categoria: string;
     nombre: string;
     descripcion: string;
